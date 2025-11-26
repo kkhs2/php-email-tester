1f24edefda3f755e1951ef1bb1765c77f458edc2
ks <kenneth.shum@booker.co.uk>
2025-11-26 09:19:20 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -44,8 +44,7 @@ try {
         $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'kshumbooker@gmail.com';
-    $mail->Username   = 'kenneth.shum@booker.co.uk';                     //SMTP username
+    $mail->Username   = 'kshumbooker@gmail.com';                  //SMTP username
     $mail->Password   = 'lbwk cend spaz ojtw';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
