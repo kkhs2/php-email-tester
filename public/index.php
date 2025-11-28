fa56a27c1360eab633370d02dbae8e598e19165c
ks <kenneth.shum@booker.co.uk>
2025-11-28 15:26:21 +0000

    venus changes

--- public/index.php
+++ public/index.php
@@ -50,7 +50,7 @@ try {
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('noreply@bookertest.co.uk', 'Booker UAT');
+    $mail->setFrom('noreply@bookertest.co.uk', 'Venus UAT');
     //$mail->addAddress('kenneth.shum@booker.co.uk');     //Add a recipient
     $mail->addAddress('kshumbooker@gmail.com');     //Add a recipient
     $mail->addAddress('kenneth.shum@booker.co.uk');
