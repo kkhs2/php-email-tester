d43a3b50b3290e2676125bd3e17b471713804627
ks <kenneth.shum@booker.co.uk>
2025-12-01 14:07:17 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -36,7 +36,7 @@ require '../vendor/autoload.php';
 $mail = new PHPMailer(true);
 
 try {
-    $dir = __DIR__ . '/../templates/emails/booker/password';
+    $dir = __DIR__ . '/../templates/emails/booker/contactus';
     $emails = scandir($dir);
     foreach ($emails as $email) {
       if (substr($email, -5) == '.html') {
