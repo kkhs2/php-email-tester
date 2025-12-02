753471e52ed94357174f8a01c1b7a20bb24cda34
kkhs2 <shumkhk@gmail.com>
2025-12-02 07:12:16 +0000

    major refactor

--- public/index.php
+++ public/index.php
@@ -32,47 +32,55 @@ use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader (created by composer, not included with PHPMailer)
 require '../vendor/autoload.php';
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
 
-try {
-    $dir = __DIR__ . '/../templates/emails/booker/accountcancelled';
-    
-    
-    $emails = scandir($dir);
-
-    foreach ($emails as $email) {
-      if (substr($email, -5) == '.html') {
-       
-        $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'kshumbooker@gmail.com';                  //SMTP username
-    $mail->Password   = 'lbwk cend spaz ojtw';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
+function sendEmail($path, $type)
+{
+  try {
+    $fileName = pathinfo($path)['basename'];
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth = true;                                   //Enable SMTP authentication
+    $mail->Username = 'kshumbooker@gmail.com';                  //SMTP username
+    $mail->Password = 'lbwk cend spaz ojtw';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS  encryption
+    $mail->Port = 587;
     //Recipients
-    $mail->setFrom('noreply@bookertest.co.uk', 'Booker UAT');
+    $mail->setFrom('noreply@bookertest.co.uk', $type . ' UAT');
     $mail->addAddress('kenneth.shum@booker.co.uk');     //Add a recipient
     $mail->addAddress('kshumbooker@gmail.com');     //Add a recipient
-    //$mail->addAddress('websiteuat@booker.co.uk');
     $mail->addReplyTo('noreply@bookertest.co.uk', 'Information');
-    //$mail->addCC('cc@example.com');
-    //$mail->addBCC('bcc@example.com');
+
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
 
     //Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    //$mail->addAttachment('/var/tmp/file.tar.gz');       
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');  
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = $email;
-    $mail->Body = file_get_contents($dir . '/' . $email); 
+    $mail->Subject = $fileName;
+    $mail->Body = file_get_contents($path);
     $mail->send();
-    echo $email . " has been sent <br />";    
-      }
+    echo $type . " " . $fileName . " has been sent <br />";
+  } catch (Exception $e) {
+    echo " Error sending email: " . $e->getMessage();
+  }
+}
+
+$emailDirectory = new RecursiveDirectoryIterator(__DIR__ . '/../templates/emails');
+
+foreach (new RecursiveIteratorIterator($emailDirectory) as $file) {
+  if ($file->getExtension() == 'html') {
+    $path = explode('\\', $file);
+
+    if (in_array("booker", $path)) {
+      $type = "Booker";
     }
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    if (in_array("venus", $path)) {
+      $type = "Venus";
+    }
+    sendEmail($file, $type);
+  }
 }
