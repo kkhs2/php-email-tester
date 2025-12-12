45a4ff2c055255ff29456f4f42c7397162838fb3
kkhs2 <shumkhk@gmail.com>
2025-12-12 14:58:08 +0000

    BookerRegisteredThankYou_noPdf fix

--- public/emails/BookerRegisteredThankYou_noPdf.html.php
+++ public/emails/BookerRegisteredThankYou_noPdf.html.php
@@ -21,7 +21,7 @@
   <?php include '../components/tdstyle.html.php' ?>
 
   <p>
-    Your customer number is <strong>{CUSTOMERNUMBER}</strong>,
+    Your customer number is <strong>{CUSTOMERNUMBER}</strong>.
   </p>
   </td>
 </tr>
@@ -54,6 +54,20 @@
 
     <p>
       If this does not work, please cut and paste the link above into the browser's address bar, and click enter.
+    </p>
+  </td>
+</tr>
+
+<tr>
+  <?php include '../components/tdstyle.html.php' ?>
+
+    <p>
+      Please login to <!--[if mso]>
+<a style="text-decoration: underline; color: #000; font-weight: 700" href="https://www.booker.co.uk">booker.co.uk</a>
+<![endif]--><!--[if !mso> <!--><a style="text-decoration: none;
+                          border-bottom: 1px dotted #000;
+                          font-weight: 700;
+                          color: #000;" href="https://www.booker.co.uk">booker.co.uk</a><!--<![endif]--> with your login details
     </p>
   </td>
 </tr>
